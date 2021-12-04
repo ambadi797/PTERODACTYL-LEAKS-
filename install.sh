6b355e114eb0fac12c5f9d6c9e40595f384a7230
ambadi797 <81955216+ambadi797@users.noreply.github.com>
2021-12-04 10:43:39 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,10 @@
-php /var/www/pterodactyl/artisan down
-cd /var/www/pterodactyl/resources/scripts/components/server
-rm -rf ServerConsole.tsx
-rm -rf McPaste.tsx
-wget https://raw.githubusercontent.com/finnie2006/PteroFreeStuffinstaller/main/resources/McPaste/McPaste.tsx
-wget https://raw.githubusercontent.com/finnie2006/PteroFreeStuffinstaller/main/resources/McPaste/ServerConsole.tsx
+cd /var/www/pterodactyl/resources/scripts/components/server/files/UploadButton.tsx
+rm -rf UploadButton.tsx
+wget https://raw.githubusercontent.com/ambadi797/PTERODACTYL-LEAKS-/main/UploadButton.tsx
 clear
 npm i -g yarn
 cd /var/www/pterodactyl
 yarn install
 yarn run build:production
 clear
-php /var/www/pterodactyl/artisan up
-echo "McPaste Installed
+echo "UPLOAD INSTALLED"
